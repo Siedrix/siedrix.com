c37fcc0988ea42bb82ead999729d2d42d33888bb
Daniel Zavala <siedrix@gmail.com>
2016-04-17 23:12:45 -0500

    Updated to v0.9.3 of paperpress

--- server.js
+++ server.js
@@ -1,33 +1,83 @@
 var express = require('express'),
 	Paperpress = require('paperpress').Paperpress,
-	logger = require('morgan');
+	logger = require('morgan'),
+	swig = require('swig'),
+	_ = require('underscore')
+
+var feedDescription = require('./feed-description')
 
 var server = express();
 
 server.use(logger(':status :req[x-real-ip] :method :response-time ms :url'));
 
-var blog = new Paperpress({
-	directory : 'static',
-	themePath : 'static/themes/base',
-	basePath  : '/blog',
-	articlesPerPage : 20,
-	pagesPath : ''
-});
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', __dirname + '/views');
+server.set('view cache', false);
+swig.setDefaults({ cache: false });
 
-blog.attach(server);
+var blog = new Paperpress({
+	// directory : 'static',
+	// themePath : 'static/themes/base',
+	// basePath  : '/blog',
+	// articlesPerPage : 20,
+	// pagesPath : ''
+	uriPrefix: '/blog'
+});
+blog.load()
+
+server.use(express.static('public'))
 
 server.get('/', function (req, res) {
-	res.redirect('/blog');
-});
+	res.redirect('/blog')
+})
+
+server.get('/blog', function (req, res) {
+	var articles = blog.getCollection('articles')
+
+	res.render('multiple',{
+		articles: articles
+	})
+})
+
+server.get('/info', function(req, res) {
+	var article = _.findWhere(blog.items,{type:'pages', slug:'info'})
+
+	res.render('single',{
+		article: article
+	})
+})
+
+server.get('/blog/:article', function (req, res) {
+	var articles = blog.getCollection('articles')
+	var article = _.findWhere(articles,{type:'articles', slug:req.params.article})
+
+	res.render('page',{
+		article: article
+	})
+})
 
 server.get('/feed', function (req, res) {
-	res.redirect('/rss');
-});
+	res.redirect('/rss')
+})
 
-var webhook = require('./webhook');
+server.get('/rss', function (req, res) {
+	// res.send('hi')
+	var articles = blog.getCollection('articles')
+	articles.forEach((item)=>{
+		item.suggestedUri = '/blog/' + item.slug
+	})
 
-server.get('/webhook',webhook(blog));
-server.post('/webhook',webhook(blog));
+	var feed = Paperpress.helpers.createFeed(feedDescription, articles)
 
-server.listen(4000);
-console.log('Server running at http://localhost:4000', new Date() );
+	res.set('Content-Type', 'text/xml');
+	res.send(feed.render('rss-2.0'));	
+})
+
+var webhook = require('./webhook')
+
+server.get('/webhook',webhook(blog))
+server.post('/webhook',webhook(blog))
+
+server.listen(4000)
+console.log('Server running at http://localhost:4000', new Date() )
